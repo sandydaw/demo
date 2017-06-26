6312a32b27e9abc55d600ebddb14731cc968d2c2
sandydaw <sandy.dawre123@gmail.com>
2017-06-26 17:55:13 +0530

    Update Demo.java

--- demo1/src/com/san/Demo.java
+++ demo1/src/com/san/Demo.java
@@ -2,7 +2,7 @@ package com.san;
 
 public class Demo {
 	public static void main(String[] args) {
-	System.out.println("this is the demo1 project ...");
+	System.out.println("this is the demo1 project ...changes made");
 	}
 
 }
